8b609e0d82a6d91268891300a55f9239bd616f17
Gordon <gordon_chan@mediconcen.com>
2021-07-23 14:00:39 +0800

    test js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const sub = require('./sub');
+
+$(() => {
+    console.log('this is main');
+})
---
+++ js/sub.js
@@ -0,0 +1,7 @@
+const test = () => {
+    $(() => {
+        console.log("from sub");
+    })
+}
+
+export default test;
