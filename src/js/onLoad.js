f678e637a8ce66d365d3d787f4c1aae9e839dc5f
Soap Wong <68498079+wlwong703@users.noreply.github.com>
2021-07-28 01:06:25 +0800

    fix path

--- src/js/onLoad.js
+++ src/js/onLoad.js
@@ -5,11 +5,11 @@ const path =
 
 const getFilePath = (file) => `${path}${file}`;
 
-requirejs([getFilePath("js/common")], (common) => {
+requirejs([getFilePath("src/js/common")], (common) => {
   const mainDiv = common.getMainDiv();
   mainDiv.css({
     display: "flex",
   });
   mainDiv.prepend(`<div id="sideBar"></div>`);
-  $("#sideBar").load(getFilePath("component/sidebar.html"));
+  $("#sideBar").load(getFilePath("src/component/sidebar.html"));
 });
