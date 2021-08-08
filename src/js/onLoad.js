826aadb74b86b2b849c6d719a5e4047efe912de2
Soap Wong <68498079+wlwong703@users.noreply.github.com>
2021-08-08 21:31:48 +0800

    update banner prepend

--- src/js/onLoad.js
+++ src/js/onLoad.js
@@ -3,29 +3,35 @@ $(document).ready(function () {
 
   const path =
     window.location.hostname === "localhost"
-      ? "./"
-      : "https://gordonchantc.github.io/polyu-mooc-edx/src/";
+      ? "."
+      : "https://gordonchantc.github.io/polyu-mooc-edx/src";
 
   const getFilePath = (file) => `${path}${file}`;
 
   const mainDiv = $("#main");
 
+  const contentDiv = $(".vert-mod");
+
   mainDiv.css({
     display: "flex",
-    height: "1024px",
+    height: "930px",
     "overflow-y": "auto",
     "overflow-x": "hidden",
     width: "100%",
+    "justify-content": "center",
   });
   mainDiv.prepend(`<div id="sideBar">Menu Content</div>`);
   $("#sideBar").css({
     position: "sticky",
-    top: "60px",
+    top: "0px",
     left: "0px",
-    margin: "8px",
-    width: "120px",
-    height: "200px",
+    width: "260px",
+    height: "930px",
     background: "#eaeaea",
-    padding: "8px",
   });
+  contentDiv.prepend(
+    `<image width="840px" height="216px" src="${getFilePath(
+      "/images/banner/Chopped/M1B.jpg"
+    )}" />`
+  );
 });
