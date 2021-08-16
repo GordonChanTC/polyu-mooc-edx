64a292cfbf286506ae56fdf0d7a01deb37ad3bcc
Gordon <bicolorlab@gmail.com>
2021-08-16 23:59:11 +0800

    update video js & css

--- src/component/video/video.js
+++ src/component/video/video.js
@@ -1,19 +1,22 @@
 $(".m-video-preview").click((e) => {
     if (Hls.isSupported()) {
-        const video = $("#video");
+        const video = $("#video")[0];
         const hls = new Hls();
         hls.loadSource(
         "https://edx-video.net/" +
             $(e.currentTarget).data("video-id") +
             ".m3u8"
         );
-        hls.attachMedia(video[0]);
+        hls.attachMedia(video);
         $("#modal").show();
     }
 });
 
 $("#m-overlay").click((e) => {
     if ($(e.target).attr("id") === "m-overlay") {
+        const video = $("#video")[0];
+        video.pause();
+        video.currentTime = 0;
         $("#modal").hide();
     }
 });
