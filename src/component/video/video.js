ed8e5b0467af8d78383ef610a1712010eb777406
Gordon <bicolorlab@gmail.com>
2021-08-19 00:34:27 +0800

    update video html & js

--- src/component/video/video.js
+++ src/component/video/video.js
@@ -1,23 +1,24 @@
-$(".m-video-preview").click((e) => {
-    console.log(Hls.isSupported());
-    if (Hls.isSupported()) {
+$( document ).ready(function() {
+    $(".m-video-preview").click((e) => {
+        if(Hls.isSupported()) {
         const video = $("#video")[0];
         const hls = new Hls();
         hls.loadSource(
-        "https://edx-video.net/" +
+            "https://edx-video.net/" +
             $(e.currentTarget).data("video-id") +
             ".m3u8"
         );
         hls.attachMedia(video);
         $("#modal").show();
-    }
-});
+        }
+    });
 
-$("#m-overlay").click((e) => {
-    if ($(e.target).attr("id") === "m-overlay") {
-        const video = $("#video")[0];
-        video.pause();
-        video.currentTime = 0;
-        $("#modal").hide();
-    }
-});
+    $("#m-overlay").click((e) => {
+        if($(e.target).attr("id") === "m-overlay") {
+            const video = $("#video")[0];
+            video.pause();
+            video.currentTime = 0;
+            $("#modal").hide();
+        }
+    });
+})
