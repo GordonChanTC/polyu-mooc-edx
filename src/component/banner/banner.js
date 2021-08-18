52d89c4d6cf47caf167c6c0d5bba59ddda492040
Soap Wong <68498079+wlwong703@users.noreply.github.com>
2021-08-19 01:10:57 +0800

    update banner

--- src/component/banner/banner.js
+++ src/component/banner/banner.js
@@ -2,6 +2,10 @@ const initBanner = (moduleId) => {
   const contentDiv = $(".vert-mod");
 
   contentDiv.prepend(
-    `<image class="m-banner" src="https://gordonchantc.github.io/polyu-mooc-edx/src/images/banner/M${moduleId}.jpg" />`
+    `
+    <div class="vert">
+    <image class="m-banner" src="https://gordonchantc.github.io/polyu-mooc-edx/src/images/banner/M${moduleId}.jpg" />
+    </div>
+    `
   );
 };
