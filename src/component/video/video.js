0e7e0a8fc18fd2720244b12d951c51f934509c45
Soap Wong <68498079+wlwong703@users.noreply.github.com>
2021-08-27 02:04:07 +0800

    update video autoplay

--- src/component/video/video.js
+++ src/component/video/video.js
@@ -1,38 +1,36 @@
-$( document ).ready(function() {
-    $(".m-video-preview").click((e) => {
-        if(Hls.isSupported()) {
-        const video = $("#video")[0];
-        const hls = new Hls();
-        hls.loadSource(
-            "https://edx-video.net/" +
-            $(e.currentTarget).data("video-id") +
-            ".m3u8"
-        );
-        hls.attachMedia(video);
-        $("#modal").show();
-        }
-    });
+$(document).ready(function () {
+  $(".m-video-preview").click((e) => {
+    if (Hls.isSupported()) {
+      const video = $("#video")[0];
+      const hls = new Hls();
+      hls.loadSource(
+        "https://edx-video.net/" + $(e.currentTarget).data("video-id") + ".m3u8"
+      );
+      hls.attachMedia(video);
+      $("#modal").show();
+      video.autoplay = true;
+    }
+  });
 
-    $(".m-video-only-preview").click((e) => {
-        if(Hls.isSupported()) {
-        const video = $("#video")[0];
-        const hls = new Hls();
-        hls.loadSource(
-            "https://edx-video.net/" +
-            $(e.currentTarget).data("video-id") +
-            ".m3u8"
-        );
-        hls.attachMedia(video);
-        $("#modal").show();
-        }
-    });
+  $(".m-video-only-preview").click((e) => {
+    if (Hls.isSupported()) {
+      const video = $("#video")[0];
+      const hls = new Hls();
+      hls.loadSource(
+        "https://edx-video.net/" + $(e.currentTarget).data("video-id") + ".m3u8"
+      );
+      hls.attachMedia(video);
+      $("#modal").show();
+      video.autoplay = true;
+    }
+  });
 
-    $("#m-overlay").click((e) => {
-        if($(e.target).attr("id") === "m-overlay") {
-            const video = $("#video")[0];
-            video.pause();
-            video.currentTime = 0;
-            $("#modal").hide();
-        }
-    });
-})
+  $("#m-overlay").click((e) => {
+    if ($(e.target).attr("id") === "m-overlay") {
+      const video = $("#video")[0];
+      video.pause();
+      video.currentTime = 0;
+      $("#modal").hide();
+    }
+  });
+});
