5e6cec7698829615fefbd5b3c03d5867a4161d51
Gordon <bicolorlab@gmail.com>
2021-08-27 23:42:40 +0800

    update video js

--- src/component/video/video.js
+++ src/component/video/video.js
@@ -33,4 +33,34 @@ $(document).ready(function () {
       $("#modal").hide();
     }
   });
+
+  $(".m-video-preview-en").click((e) => {
+        if(Hls.isSupported()) {
+            const video = $("#video")[0];
+            const hls = new Hls();
+            hls.loadSource(
+                "https://edx-video.net/" +
+                $(e.currentTarget).data("video-id") +
+                ".m3u8"
+            );
+            hls.attachMedia(video);
+            $("#modal").show();
+            video.autoplay = true;
+        }
+    })
+
+    $(".m-video-preview-cn").click((e) => {
+        if(Hls.isSupported()) {
+            const video = $("#video")[0];
+            const hls = new Hls();
+            hls.loadSource(
+                "https://edx-video.net/" +
+                $(e.currentTarget).data("video-id") +
+                ".m3u8"
+            );
+            hls.attachMedia(video);
+            $("#modal").show();
+            video.autoplay = true;
+        }
+    })
 });
