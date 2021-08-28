80a6668b2ddc0f1fedd938e15d54eb20c1adbe0b
Gordon <bicolorlab@gmail.com>
2021-08-28 23:36:33 +0800

    make a copy of sidebar links in sidebar/chapters/legend

---
+++ src/component/sidebar/chapters/legend/ch1.js
@@ -0,0 +1,45 @@
+const modules = {
+    "title": "Module 1: Why we need to learn Design Thinking",
+    "course": [
+        {
+            "id": 1,
+            "title": "M1.1 Objective",
+            "courseId": "9c93785cbdfb42329acdacecb6a22fdf",
+        },
+        {
+            "id": 2,
+            "title": "M1.2 Introduction",
+            "courseId": "ae5b7e1231fc4b0a9b2914441d2852a0",
+        },
+        {
+            "id": 3,
+            "title": "M1.3 Traditional problem-solving approach",
+            "courseId": "4c66cf55d8e34011bf7d9a3326720d46",
+        },
+        {
+            "id": 4,
+            "title": "M1.4 Wicked Problems",
+            "courseId": "1b71790ff59f4731be259462bd1c8f4e",
+        },
+        {
+            "id": 5,
+            "title": "M1.5 Design Thinking problem-solving approach",
+            "courseId": "91181545d2f24056ba60bb0e1584da75",
+        },
+        {
+            "id": 6,
+            "title": "M1.6 Design Thinking is more than a process",
+            "courseId": "230be46b6ff3421388d3671e2b91c26a",
+        },
+        {
+            "id": 7,
+            "title": "M1.7 Module activities",
+            "courseId": "d09cc6e0eda147d2861cc81888114d5c",
+        },
+        {
+            "id": 8,
+            "title": "M1.8 Recap and Test of Module 1",
+            "courseId": "5a5b3bb2049a463b9c7467feb6dc714c",
+        }
+    ]
+}
---
+++ src/component/sidebar/chapters/legend/ch5.js
@@ -0,0 +1,30 @@
+const modules = {
+  title: "Module 5: How to become a design thinker?",
+  course: [
+    {
+      id: 1,
+      title: "M5.1 Objective",
+      courseId: "9acb020dd14b44db94f2e0e339b60ec7",
+    },
+    {
+      id: 2,
+      title: "M5.2 Introduction",
+      courseId: "d573bd32e32848a5aa7f036240f1c5d5",
+    },
+    {
+      id: 3,
+      title: "M5.3 Characteristics of design thinker",
+      courseId: "de93d582be1249b9bf6135548f574d3a",
+    },
+    {
+      id: 4,
+      title: "M5.4 Module activities",
+      courseId: "c1a320b67c394c979af81f6bd9df29de",
+    },
+    {
+      id: 5,
+      title: "M5.5 Recap of Module 5",
+      courseId: "ffe4225eb1fd49fba667f5c777e1fb1b",
+    },
+  ],
+};
